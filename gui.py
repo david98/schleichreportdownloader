9f4bc83b71d2de5729e4b66f58baa9a1e78468a7
david98 <davidevolta98@gmail.com>
2019-05-20 12:19:19 +0200

    Refactoring

--- gui.py
+++ gui.py
@@ -11,7 +11,7 @@ class UiMainWindow(object):
         self.central_widget = None
         self.vertical_layout = None
         self.start_test_button = None
-        self.status_info = None
+        self.text_box = None
         self.status_labels = None
         self.loading_icon = None
         self.connection_status = None
@@ -22,7 +22,7 @@ class UiMainWindow(object):
 
     @QtCore.pyqtSlot(str)
     def on_text_feedback_update(self, new_text: str):
-        self.status_info.setText(new_text)
+        self.text_box.setText(new_text)
 
     @QtCore.pyqtSlot(str)
     def on_status_feedback_update(self, new_text: str):
@@ -59,13 +59,13 @@ class UiMainWindow(object):
 
         self.test_manager.start_test_control.set_start_test_enable.connect(self.on_set_start_test_enable)
 
-        self.status_info = QtWidgets.QTextEdit(self.central_widget)
+        self.text_box = QtWidgets.QTextEdit(self.central_widget)
 
         self.test_manager.text_feedback.text_feedback_update.connect(self.on_text_feedback_update)
 
-        self.status_info.setReadOnly(True)
-        self.status_info.setObjectName("statusInfo")
-        self.vertical_layout.addWidget(self.status_info)
+        self.text_box.setReadOnly(True)
+        self.text_box.setObjectName("statusInfo")
+        self.vertical_layout.addWidget(self.text_box)
         self.status_labels = QtWidgets.QHBoxLayout()
         self.status_labels.setObjectName("status_labels")
         self.loading_icon = QtWidgets.QLabel(self.central_widget)
@@ -111,7 +111,7 @@ class UiMainWindow(object):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "Schleich Report Downloader"))
         self.start_test_button.setText(_translate("MainWindow", "Start Test"))
-        self.status_info.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+        self.text_box.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:\'Noto Sans\'; font-size:10pt; font-weight:400; font-style:normal;\">\n"
